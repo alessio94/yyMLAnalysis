42df8941a5f785b216937bbe8d26c655e94e242f
Konstantin Lehmann <klehmann@sfu.ca>
2019-10-17 17:07:31 +0200

    Revert "Changed script for debugging. Revert this commit or fix in another way later."
    This reverts commit b48d9ab7849882c1a9899e740b8c3c3fef5c9e9b.

--- tools/CI_checks.sh
+++ tools/CI_checks.sh
@@ -3,33 +3,36 @@
 # This script searches all files that match share/config/samples/maps/*/*.map
 # and prints a warning if they have overlapping/duplicate DSID mappings
 
-sed +e
 
-listOfDuplicateDSIDs=""
+HWWAnalysis_checkMapFiles(){
 
-mapsPath="share/config/samples/maps/*/*.map"
+    listOfDuplicateDSIDs=""
 
-# CAFANALYSISBASE might not be set in a CI job. In that case, hope that we are in the right location
-for file in `[ -z "$CAFANALYSISBASE" ] && ls $mapsPath || ls $CAFANALYSISBASE/$mapsPath` ; do
-    for line_orig in `cat $file | sed 's/ /_/g'` ; do
+    mapsPath="share/config/samples/maps/*/*.map"
 
-        # ignore spaces in the beginning
-        line=$(echo "$line_orig" | sed 's/^\(_\| \)*//')
+    # CAFANALYSISBASE might not be set in a CI job. In that case, hope that we are in the right location
+    for file in `[ -z "$CAFANALYSISBASE" ] && ls $mapsPath || ls $CAFANALYSISBASE/$mapsPath` ; do
+        for line_orig in `cat $file | sed 's/ /_/g'` ; do
 
-        # if the line starts with #, ignore it
-        echo "$line" | sed '/^#/{q10}' > /dev/null
-        [[ $? -eq 10 ]] && continue
+            # ignore spaces in the beginning
+            line=$(echo "$line_orig" | sed 's/^\(_\| \)*//')
 
-        # search for 6-digit (maybe with extension like "w10") DSID in $file
-        DSID=$(echo $line | sed 's/\([0-9]\{6\}\)\(w[0-9]\{2\}\)\?\(_\| \).*/\1\2/')
-        nMatches=$(grep -r "$DSID" $file | grep -v "^ *#" | wc -l)
+            # if the line starts with #, ignore it
+            echo "$line" | sed '/^#/{q10}' > /dev/null
+            [[ $? -eq 10 ]] && continue
 
-        # if more than one line matches this DSID and the DSID hasn't been reported before, add it to the list of DSIDs and print warning
-        ! [[ nMatches -eq 1 ]] && ! [[ $(echo "$listOfDuplicateDSIDs" | grep "$DSID") ]] && listOfDuplicateDSIDs="$listOfDuplicateDSIDs,$DSID" && echo "duplicate: $DSID, file: $file" > /dev/stderr && grep -r "$DSID" $file > /dev/stderr
+            # search for 6-digit (maybe with extension like "w10") DSID in $file
+            DSID=$(echo $line | sed 's/\([0-9]\{6\}\)\(w[0-9]\{2\}\)\?\(_\| \).*/\1\2/')
+            nMatches=$(grep -r "$DSID" $file | grep -v "^ *#" | wc -l)
 
+            # if more than one line matches this DSID and the DSID hasn't been reported before, add it to the list of DSIDs and print warning
+            ! [[ nMatches -eq 1 ]] && ! [[ $(echo "$listOfDuplicateDSIDs" | grep "$DSID") ]] && listOfDuplicateDSIDs="$listOfDuplicateDSIDs,$DSID" && echo "duplicate: $DSID, file: $file" > /dev/stderr && grep -r "$DSID" $file > /dev/stderr
+
+        done
     done
-done
 
-set -e
+    set -e
 
-[ "$listOfDuplicateDSIDs" == "" ]
+    [ "$listOfDuplicateDSIDs" == "" ]
+    return $?
+}
